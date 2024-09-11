9499669a76b60c84012434782767dbfdf31ad600
Saidakrom Rakhmedov <s.raxmedov@hayotbank.uz>
2024-09-11 20:28:20 +0500

    fix

--- components/ui/Modal/index.tsx
+++ components/ui/Modal/index.tsx
@@ -1,9 +1,8 @@
 import RSModal, { ModalProps } from 'rsuite/Modal';
 import './styles.scss';
-import { FC, useEffect } from 'react';
+import { FC } from 'react';
 import { Icon } from '../Icon';
 import React from 'react';
-import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
 
 interface IProps extends ModalProps {
     title?: string;
@@ -11,15 +10,7 @@ interface IProps extends ModalProps {
 
 export const Modal: FC<IProps> = ({ title, children, onClose, ...props }) => {
     const ref = React.useRef<HTMLElement>(null);
-    // useEffect(() => {
-    //     if (props.open) {
-    //         disableBodyScroll(ref.current!);
-    //     }
-    //     return () => {
-    //         enableBodyScroll(ref.current!);
-    //     };
-    // }, [props.open])
-    return <RSModal {...props} onClose={onClose} ref={ref} overflow>
+    return <RSModal {...props} onClose={onClose} ref={ref}>
         <Icon name="close-circle" size={24} onClick={onClose} className={'rs-modal-close'} />
         <div className={'rs-modal-content-inner'}>
             <div className={'rs-modal-title'}>{title}</div>
