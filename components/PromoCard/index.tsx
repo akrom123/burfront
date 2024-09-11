ad549c36e004b4654c8e3657748d08ba7c580d5a
Saidakrom Rakhmedov <s.raxmedov@hayotbank.uz>
2024-09-11 21:59:25 +0500

    fix

--- components/PromoCard/index.tsx
+++ components/PromoCard/index.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { FC } from "react";
 import styles from './styles.module.scss'
 import { Button } from "../ui/Button";
@@ -11,12 +13,12 @@ interface IPromoCardProps {
 
 export const PromoCard: FC<IPromoCardProps> = ({ title, description, background = '#FBC04E00' }) => {
     const router = useRouter()
-    
+
     return <div className={styles.promoCard} style={{ background }}>
         <div className={styles.promoCardContent}>
             <div className={styles.promoCardTitle}>{title}</div>
             <div className={styles.promoCardDescription}>{description}</div>
         </div>
-        <Button variant={'transparent'}  onClick={() => router.push('/promo')}>Подробнее</Button>
+        <Button variant={'transparent'} onClick={() => router.push('/promo')}>Подробнее</Button>
     </div>
 }
